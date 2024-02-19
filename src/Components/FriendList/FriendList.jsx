c35d53881da101908511f250396ec35c653a05aa
Maxim <kmaxusk@gmail.com>
2024-02-19 16:10:04 +0200

    without-styles

---
+++ src/Components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem friend={friend} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default FriendList;
